f19a1ed56dcaec641855f3d9c4f4ff9f5fd91c9d
Folka2134 <folka2134@gmail.com>
2023-12-07 11:01:22 +0000

    updated extractPrice function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,21 @@
 export function extractPrice(...elements: any) {
-  for(const element of elements) {
-    const priceText = element.text().trim()    
+  for (const element of elements) {
+    const priceText = element.text().trim();
 
     if(priceText) {
-      return priceText.replace(/[^\d.]/g, "")
+      const cleanPrice = priceText.replace(/[^\d.]/g, '');
+
+      let firstPrice; 
+
+      if (cleanPrice) {
+        firstPrice = cleanPrice.match(/\d+\.\d{2}/)?.[0];
+      } 
+
+      return firstPrice || cleanPrice;
     }
   }
-  return ""
+
+  return '';
 }
 
 export function extractCurrency(element: any) {
